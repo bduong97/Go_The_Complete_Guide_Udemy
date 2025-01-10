c037c1833cbf8ae82d1128cf3727c5dea9d950f8
brduong <brduong@cisco.com>
2025-01-09 18:24:23 -0800

    added functions to profit calculator

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -5,22 +5,27 @@ import "fmt"
 func main () {
 	var revenue, expenses, taxRate float64
 
-	fmt.Print("Input revenue: ")
-	fmt.Scan(&revenue)
-	
-	fmt.Print("Input expenses: ")
-	fmt.Scan(&expenses)
+	revenue = getUserInput("Input revenue: ")
+	expenses = getUserInput("Input expenses: ")
+	taxRate = getUserInput("Input taxRate: ")
 
-	fmt.Print("Input taxRate: ")
-	fmt.Scan(&taxRate)
+	ebt, profit, ratio := finalCalcs(revenue, expenses, taxRate)
 
+	fmt.Printf("ebt: %.1f\n", ebt)
+	fmt.Printf("profit: %.1f\n", profit)
+	fmt.Printf("ratio: %.3f\n", ratio)
+}
+
+func getUserInput(infoText string) float64 {
+	var val float64
+	fmt.Print(infoText)
+	fmt.Scan(&val)
+	return val
+}
+func finalCalcs(revenue, expenses, taxRate float64 ) (float64, float64, float64) {
 	ebt:=  revenue - expenses
 	profit:= ebt * (1 - (taxRate/100)) 
 	ratio := ebt/profit
-
-	fmt.Println("ebt:", ebt)
-	fmt.Println("profit:", profit)
-	fmt.Println("ratio:", ratio)
-
+	return ebt, profit, ratio
 
 }
