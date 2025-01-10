b3d343b816c15a01e0058e740db42e3cb91ac143
brduong <brduong@cisco.com>
2025-01-09 17:16:40 -0800

    Adding user input to calculator investment

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,24 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000 //Multiple variable declaration
-	var years float64 = 10 //Better readability 
+	const inflationRate float64 = 2.5
+	var investmentAmount float64 
+	var years float64 //Better readability 
 	expectedReturnRate := 5.5 //When happy with inferred type, you can use a shorter way to assign a variable without var
 
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	fmt.Print("Input your investment amount: ")
+	fmt.Scan(&investmentAmount) //scan only accepts pointers
+	//fmt.Scan(&inflationRate) //errors out because constants can't change
+	
+	fmt.Print("Input your years: ")
+	fmt.Scan(&years) //scan only accepts pointers
+	
+	fmt.Print("Input your expected return rate: ")
+	fmt.Scan(&expectedReturnRate) //scan only accepts pointers
+	
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
