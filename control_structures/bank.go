ffc446db7fa03603c3f8d47aa22d20484a03b353
brduong <brduong@cisco.com>
2025-01-10 17:54:16 -0800

    Switch statements

--- control_structures/bank.go
+++ control_structures/bank.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main ()  {
+func main() {
 	var accountBalance = 1000.0
 	fmt.Println("Welcome to Go Bank!")
 	for { //while loop
@@ -12,42 +12,43 @@ func main ()  {
 		fmt.Println("3. Withdraw money")
 		fmt.Println("4. Exit")
 
-		var choice int 
+		var choice int
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
 
-		if choice == 1 {
+		switch choice {
+		case 1:
 			fmt.Println("Your balance is", accountBalance)
-		} else if choice == 2 {
+		case 2:
 			fmt.Print("Your deposit: ")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
 			if depositAmount <= 0 {
 				fmt.Println("Invalid amount. Must be greater than 0.")
-				return
+				continue
 			}
 			accountBalance += depositAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-		} else if choice == 3 {
+		case 3:
 			fmt.Print("Withdrawl amount: ")
 			var withdrawalAmount float64
 			fmt.Scan(&withdrawalAmount)
 			if withdrawalAmount <= 0 {
 				fmt.Println("Invalid amount. Must be greater than 0.")
-				return
+				continue
 			}
 			if withdrawalAmount > accountBalance {
 				fmt.Println("Invalid amount. Withdrawal amount must be less than account balance")
-				return
+				continue
 			}
 			accountBalance -= withdrawalAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-		} else {
+		default:
+			fmt.Println("Thanks for choosing our bank")
 			fmt.Println("Goodbye")
+			return
+
 		}
-		
-		
-		fmt.Println("Your choice:", choice)
 	}
-	
+
 }
