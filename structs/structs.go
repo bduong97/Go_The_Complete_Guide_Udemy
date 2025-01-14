11b0d07d563b36f0c83ffda08570839ceaaf0ef2
brduong <brduong@cisco.com>
2025-01-14 11:32:59 -0800

    embedded structs

--- structs/structs.go
+++ structs/structs.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
 	"example.com/structs/user"
 )
 
@@ -12,13 +10,19 @@ func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
-	appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthdate)
 	// ... do something awesome with that gathered data!
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	//outputUserDetails(&appUser)
+	admin := user.NewAdmin("test@example.com", "test123")
+
+	admin.User.OutputUserDetails()
+	admin.User.ClearUserName()
+	admin.User.OutputUserDetails()
+
 	appUser.OutputUserDetails()
 	appUser.ClearUserName()
 	appUser.OutputUserDetails()
--- structs/user/user.go
+++ structs/user/user.go
@@ -1,5 +1,6 @@
 package user
 
+//Making a var/method publically accessible, make sure it's capitalized 
 import (
 	"errors"
 	"fmt"
@@ -13,6 +14,24 @@ type User struct {
 	createdAt time.Time
 }
 
+type Admin struct {
+	email string
+	password string
+	User User
+}
+
+func NewAdmin(email, password string) Admin {
+	return Admin {
+		email: email,
+		password: password,
+		User: User{
+			firstName: "ADMIN",
+			lastName: "ADMIN",
+			birthDate: "---",
+			createdAt: time.Now(),
+		},
+	}
+}
 func (u User) OutputUserDetails() {
 	//pointers to structs in go don't need dereferencing
 	fmt.Println(u.firstName, u.lastName, u.birthDate)
@@ -23,7 +42,7 @@ func (u *User) ClearUserName() { //when editing variables related to structs, ne
 	u.lastName = ""
 }
 
-func NewUser(firstName, lastName, birthDate string) (*User, error) { //constructor function
+func New(firstName, lastName, birthDate string) (*User, error) { //constructor function
 	//constructor functions are useful if you want to run some validation logic when creating struct
 	if firstName == "" || lastName == "" || birthDate == "" {
 		return nil, errors.New("first name, last name and birthdate are requried")
