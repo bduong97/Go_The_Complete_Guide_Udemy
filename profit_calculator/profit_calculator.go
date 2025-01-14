4a25e0a2875ecd045e9927f4c8ee26e2be26948c
brduong <brduong@cisco.com>
2025-01-13 16:09:01 -0800

    Writing results to file

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -1,31 +1,57 @@
 package main
 
 import "fmt"
+import "errors"
+import "os"
 
-func main () {
-	var revenue, expenses, taxRate float64
+// Goals
+// 1) Validate user input
+// 	=> show error message & exit if invalid input is provided
+// 	- No negative numbers
+// 	- Not 0
+// 2) Store calculated results into file
 
-	revenue = getUserInput("Input revenue: ")
-	expenses = getUserInput("Input expenses: ")
-	taxRate = getUserInput("Input taxRate: ")
+func main() {
+
+	revenue, err := getUserInput("Input revenue: ") 
+	if err != nil { // multiple if statements if you want to output more specific error message depending on which input was incorrect
+		fmt.Println(err)
+		return
+	}
+	expenses, err := getUserInput("Input expenses: ")
+	if err != nil { // multiple if statements if you want to output more specific error message depending on which input was incorrect
+		fmt.Println(err)
+		return
+	}
+	taxRate, err := getUserInput("Input taxRate: ")
+	if err != nil { // multiple if statements if you want to output more specific error message depending on which input was incorrect
+		fmt.Println(err)
+		return
+	}
+
+	//if err1 != nil || err2 != nil || err3 != nil { //useful if all the errors will be the same anyways; 
+	//	fmt.Println(err1)
+	//	return
+	//}
 
 	ebt, profit, ratio := finalCalcs(revenue, expenses, taxRate)
-
-	fmt.Printf("ebt: %.1f\n", ebt)
-	fmt.Printf("profit: %.1f\n", profit)
-	fmt.Printf("ratio: %.3f\n", ratio)
+	resultsText := fmt.Sprintf("ebt: %.1f\nprofit: %.1f\nratio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("profit_calc.txt", []byte(resultsText), 0644)
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var val float64
 	fmt.Print(infoText)
 	fmt.Scan(&val)
-	return val
+	if val <= 0 {
+		return 0, errors.New("input must be must be greater than 0")
+	}
+	return val, nil
 }
-func finalCalcs(revenue, expenses, taxRate float64 ) (float64, float64, float64) {
-	ebt:=  revenue - expenses
-	profit:= ebt * (1 - (taxRate/100)) 
-	ratio := ebt/profit
+func finalCalcs(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - (taxRate / 100))
+	ratio := ebt / profit
 	return ebt, profit, ratio
 
 }
