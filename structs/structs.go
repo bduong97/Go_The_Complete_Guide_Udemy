642543b1c264dc7f6a6ed2e098eb39c18c2cf648
brduong <brduong@cisco.com>
2025-01-13 17:50:28 -0800

    basic struct aand pointers

---
+++ structs/structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName string
+	birthDate string
+	age int
+	createdAt time.Time
+}
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	var appUser user
+	appUser = user{
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
+	// ... do something awesome with that gathered data!
+
+	outputUserDetails(&appUser)
+}
+
+func outputUserDetails(u *user) {
+	//pointers to structs in go don't need dereferencing
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
