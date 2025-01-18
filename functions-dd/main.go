3495d50d5d5713121376c7f533e213710d0099f5
brduong <brduong@cisco.com>
2025-01-17 16:05:55 -0800

    functions as return types

--- functions-dd/main.go
+++ functions-dd/main.go
@@ -6,10 +6,20 @@ type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double) //functions as parameters
 	tripled := transformNumbers(&numbers, triple) //functions as parameters
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -18,6 +28,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers = append(dNumbers, transform(val))
 	}
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[] int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	}else {
+		return triple
+	}
 }
 
 func double(number int) int {
