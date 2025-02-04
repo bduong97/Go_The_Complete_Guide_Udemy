0214800922cc6bdb6ab0b1012fb7809f4e9f30c1
brduong <brduong@cisco.com>
2025-02-04 13:51:45 -0800

    defer keyword

--- concurrency/filemanager/filemanager.go
+++ concurrency/filemanager/filemanager.go
@@ -20,6 +20,7 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("Failed to open file.")
 	}
 
+	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -31,11 +32,11 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		file.Close()
+		//file.Close()
 		return nil, errors.New("Failed to read line in file.")
 	}
 
-	file.Close()
+	//file.Close()
 	return lines, nil
 }
 
@@ -45,17 +46,16 @@ func (fm FileManager) WriteResult(data interface{}) error {
 	if err != nil {
 		return errors.New("Failed to create file.")
 	}
+	defer file.Close()
 
 	time.Sleep(3 * time.Second)
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
-		file.Close()
 		return errors.New("Faild to convert data to JSON.")
 	}
 
-	file.Close()
 	return nil
 }
 
